46857afb6c485f028cc241a8408867489aae2345
Hamed Rezaee <13097270+hamed-rezaee@users.noreply.github.com>
2020-06-27 14:26:52 +0430

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_practical/pages/home_page.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
